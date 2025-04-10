864c0e86ff12250c5e7145443baa0955ed350512
finnq <qdafengzi@gmail.com>
2025-04-10 19:05:29 +0800

    add list component

--- src/pages/component/details/ListDetailScreen.tsx
+++ src/pages/component/details/ListDetailScreen.tsx
@@ -1,12 +1,59 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {useTheme} from '../../../theme/ThemeContext';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {FlashList} from '@shopify/flash-list';
+
+
+interface ListItemProps {
+    id: string;
+    title: string;
+}
+
+const ListItem: React.FC<ListItemProps> = (item) => (
+    <View style={{
+        height: 60,
+        backgroundColor: '#333333',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 5,
+        marginHorizontal: 16,
+        borderRadius: 10,
+    }}>
+        <Text style={{color: '#fff', width: '100%', marginLeft: 10}}>
+            {item.title}
+        </Text>
+    </View>
+);
+
+
 
 export const ListDetailScreen: React.FC = () => {
     const {colors} = useTheme();
+    const [listData, setListData] = useState<ListItemProps[]>([]);
+
+    useEffect(() => {
+        let list = [];
+        for (let i = 0; i < 300; i++) {
+            const item: ListItemProps = {
+                id: i.toString(),
+                title: `Item ${i}`,
+            };
+            list.push(item);
+        }
+        setListData(list);
+    }, []);
+
+
+
     return (
         <View style={[styles.detailContainer, {backgroundColor: colors.background}]}>
-            <Text style={{color: colors.text, fontSize: 20, fontWeight: 'bold'}}>列表组件详情</Text>
+            <FlashList
+                style={{flex: 1, width: '100%'}}
+                data={listData}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => <ListItem {...item} />}
+                estimatedItemSize={200}
+            />
         </View>
     );
 };
@@ -34,8 +81,6 @@ const styles = StyleSheet.create({
     },
     detailContainer: {
         flex: 1,
-        padding: 16,
-        alignItems: 'center',
     },
     button: {
         backgroundColor: '#f0f0f0',
