259670b1aa75515095149cfe2a30824e60b92514
finnq <qdafengzi@gmail.com>
2025-04-22 17:02:02 +0800

    useMemo使用

---
+++ src/pages/other/OtherListScreen.tsx
@@ -0,0 +1,84 @@
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useTheme} from '../../theme/ThemeContext';
+import React from 'react';
+
+/// 定义组件列表数据
+const otherList = [
+    {id: '1', title: 'useState', route: 'useState'},
+
+];
+
+// 定义导航参数类型
+export type ComponentStackParamList = {
+    OtherList: undefined;
+    useState: undefined;
+};
+
+
+// 组件列表页面
+export const OtherListScreen: React.FC<{ navigation: any }> = ({navigation}) => {
+    // 获取当前主题颜色
+    const {colors} = useTheme();
+
+    // 渲染列表项
+    const renderItem = ({item}: { item: { id: string; title: string; route: string } }) => (
+        <TouchableOpacity
+            style={[styles.card, {backgroundColor: colors.card, borderColor: colors.border}]}
+            onPress={() => navigation.navigate(item.route)}
+        >
+            <Text style={{color: colors.text}}>{item.title}</Text>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={[styles.container, {backgroundColor: colors.background}]}>
+            <Text style={[styles.header, {color: colors.text}]}>Other列表</Text>
+            <FlatList
+                data={otherList}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                contentContainerStyle={styles.listContainer}
+            />
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 16,
+        textAlign: 'center',
+    },
+    listContainer: {
+        padding: 16,
+    },
+    card: {
+        padding: 16,
+        margin: 8,
+        borderRadius: 8,
+        borderWidth: 1,
+        alignItems: 'center',
+    },
+    detailContainer: {
+        flex: 1,
+        padding: 16,
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#f0f0f0',
+        padding: 12,
+        borderRadius: 8,
+        marginVertical: 8,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '500',
+    },
+});
---
+++ src/pages/other/UseStateScreen.tsx
@@ -0,0 +1,159 @@
+import {createThemedStyles, useTheme} from '../../theme/ThemeContext';
+import {Button, Text, View} from 'react-native';
+import React, {memo, useCallback, useEffect, useMemo, useRef, useState} from 'react';
+
+/**
+ * 外部的组件 不会刷新
+ * @returns
+ */
+const HeaderViewOuter = memo(() => {
+    console.log('HeaderViewOuter 不会刷新');
+    return <View style={{height: 40, backgroundColor: 'blue', justifyContent: 'center'}}>
+        <Text style={{color: 'white'}}>外部 HeaderViewOuter 不会刷新</Text>
+    </View>;
+});
+
+export function UseStateScreen(): React.JSX.Element {
+    const {colors} = useTheme();
+    const styles = useStyles();
+    const [number, setNumber] = useState(0)
+    const [count, setCount] = useState(0)
+    const numberRef = useRef(0);
+
+    useEffect(() => {
+        console.log('页面加载\n\n');
+        console.log('  ');
+    });
+
+
+    // 使用 useCallback 记忆化 HeaderView 组件
+    const HeaderViewInner1 = useCallback(() => {
+        console.log('内部 HeaderViewInner1 会刷新 ');
+        return <View style={styles.box}>
+            <Text style={styles.text}>内部 HeaderViewInner1 会刷新 值也变化 {number}</Text>
+        </View>;
+    }, [number]);
+
+    const HeaderViewInner2 = useCallback(() => {
+        console.log('内部 HeaderViewInner2 会刷新 但number 值不变 ');
+        return <View style={styles.box}>
+            <Text style={styles.text}>内部 HeaderViewInner2 会刷新 但number 值不变 {number}</Text>
+        </View>;
+    }, []);
+
+
+    /**
+     * 当依赖有值时 会刷新
+     */
+    const headerView1 = useMemo(() => {
+        console.log('HeaderView1');
+        return <View style={styles.box}>
+            <Text style={styles.text}>header 有依赖 会刷新{number}</Text>
+        </View>;
+    }, [number]);
+
+
+    /**
+     * 当依赖为空时 不会刷新 即使引用了 state 也不会刷新
+     */
+    const headerView2 = useMemo(() => {
+        console.log('HeaderView2');
+        return <View style={styles.box}>
+            <Text style={styles.text}>header 依赖为空 不会刷新 {number}</Text>
+        </View>;
+    }, []);
+
+    /**
+     * 当依赖为Ref类型的时候 页面会刷新
+     */
+    const headerView3 = useMemo(() => {
+        console.log('headerView3');
+        return <View style={styles.box}>
+            <Text style={styles.text}>header 依赖Ref类型的是否会变化 {numberRef.current}</Text>
+        </View>;
+    }, [numberRef.current]);
+
+    /**
+     * 当没有依赖就不会属性 无论是 Ref 还是 state
+     */
+    const headerView4 = useMemo(() => {
+        console.log('headerView4');
+        return <View style={styles.box}>
+            <Text style={styles.text}>header 不依赖Ref类型的是否会变化 {numberRef.current}</Text>
+        </View>;
+    }, []);
+
+
+    const headerView5Child = useMemo(() => {
+        console.log('headerView5Child');
+        return <View style={styles.box}>
+            <Text style={styles.text}>headerView5Child count: {count}</Text>
+        </View>;
+    }, [count]);
+
+    const headerView5Child1 = useMemo(() => {
+        console.log('headerView5Child');
+        return <View style={styles.box}>
+            <Text style={styles.text}>headerView5Child1 count: {count}</Text>
+        </View>;
+    }, []);
+
+    const headerView5 = useMemo(() => {
+        console.log('headerView4');
+        return <View style={{backgroundColor: '#ff00ff'}}>
+            {headerView5Child}
+            {headerView5Child1}
+            <Text style={styles.text}>header 不依赖Ref类型的是否会变化 number:{number}</Text>
+        </View>;
+    }, [number]);
+
+
+    function addNumber() {
+        setNumber(number + 1);
+        numberRef.current = number + 1;
+        setCount(count + 1);
+    }
+
+
+    return (
+        <View style={styles.screen}>
+            <HeaderViewOuter/>
+
+            <HeaderViewInner1/>
+            <HeaderViewInner2/>
+
+            {headerView1}
+            {headerView2}
+            {headerView3}
+            {headerView4}
+            {headerView5}
+
+            <Text style={{color: colors.primary}}>
+                Other content number:{number}
+            </Text>
+
+            {/* <Button title="增加" onPress={() => setNumber(number + 1)} /> */}
+            <Button title="增加" onPress={addNumber}/>
+            <Button title="一直设置一样的数据是否变化 结果是不刷新页面" onPress={() => setNumber(99)}/>
+        </View>
+    );
+}
+
+
+const useStyles = createThemedStyles((_) => ({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    box: {
+        marginTop: 10,
+        justifyContent: 'center',
+        height: 40,
+        backgroundColor: 'blue',
+    },
+    text: {
+        color: 'white',
+    },
+}));
