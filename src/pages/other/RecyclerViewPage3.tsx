af8bd7151f2df4e9453ae87bba32115c03eecdc3
finnq <qdafengzi@gmail.com>
2026-04-01 14:56:03 +0800

    update

--- src/pages/other/RecyclerViewPage3.tsx
+++ src/pages/other/RecyclerViewPage3.tsx
@@ -1,12 +1,16 @@
-import React, {useMemo, useRef} from "react";
-import {Button, Dimensions, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React, {useMemo, useRef, useState} from "react";
+import {Button, Dimensions, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {DataProvider, LayoutProvider, RecyclerListView} from "recyclerlistview";
 import XImage from "../component/XImage.tsx";
-import {ScrollEvent} from "recyclerlistview/src/core/scrollcomponent/BaseScrollView.tsx";
+import {ScrollEvent, ScrollViewDefaultProps} from "recyclerlistview/src/core/scrollcomponent/BaseScrollView.tsx";
 import {imageDataList} from "../../data/Constant.ts";
 import {WINDOW_WIDTH} from "@gorhom/bottom-sheet";
+import {OnRecreateParams} from "recyclerlistview/src/core/RecyclerListView.tsx";
+import {TOnItemStatusChanged} from "recyclerlistview/src/core/ViewabilityTracker.ts";
+import {PullToRefresh} from "@sdcx/pull-to-refresh";
 
 enum ViewTypes {
+    HEADER = 0,
     HALF_LEFT = 1,
 }
 
@@ -14,17 +18,12 @@ type RowData = string;
 
 const RecyclerViewPage3: React.FC = () => {
     const {width} = Dimensions.get("window");
-    const scrollRef = useRef<any>();
+    const scrollRef = useRef<RecyclerListView<any, any>>(null);
+    const [refreshing, setRefreshing] = useState(false);
 
     const dataProvider = useMemo(() => {
         const dp = new DataProvider((r1: RowData, r2: RowData) => r1 !== r2);
         const rows: RowData[] = imageDataList.slice(0, 50).map((item) => item)
-        return dp.cloneWithRows(rows);
-    }, []);
-
-    const dataProvider2 = useMemo(() => {
-        const dp = new DataProvider((r1: RowData, r2: RowData) => r1 !== r2);
-        const rows: RowData[] = imageDataList.slice(10, 20).map((item) => item)
         return dp.cloneWithRows(rows);
     }, []);
 
@@ -40,17 +39,8 @@ const RecyclerViewPage3: React.FC = () => {
         );
     }, [width]);
 
-    const layoutProvider2 = useMemo(() => {
-        return new LayoutProvider(
-            (index: number) => {
-                return ViewTypes.HALF_LEFT
-            },
-            (type: number | string, dim: { width: number; height: number }) => {
-                dim.width = width;
-                dim.height = width;
-            }
-        );
-    }, [width]);
+
+
 
     const rowRenderer = (type: number | string, data: RowData): React.ReactElement | null => {
         return (
@@ -62,31 +52,70 @@ const RecyclerViewPage3: React.FC = () => {
         )
     };
 
+
+    const dataProvider2 = useMemo(() => {
+        const dp = new DataProvider((r1: RowData, r2: RowData) => r1 !== r2);
+        const rows: RowData[] = imageDataList
+            .slice(0, 10)
+            .map((item) => item)
+        return dp.cloneWithRows(rows);
+    }, []);
+
+    const layoutProvider2 = useMemo(() => {
+        return new LayoutProvider(
+            (index: number) => {
+                if (index === 0) {
+                    return ViewTypes.HEADER;
+                }
+                return ViewTypes.HALF_LEFT;
+            },
+            (type: number | string, dim: { width: number; height: number }) => {
+                if (type === ViewTypes.HEADER) {
+                    dim.width = width;
+                    dim.height = 50;
+                } else {
+                    dim.width = width;
+                    dim.height = width;
+                }
+            }
+        );
+    }, [width]);
+
+    const Header = () => <View style={styles.header}>
+        <Text style={{color: 'white', width: 'auto', textAlign: 'center', alignSelf: "center"}}>头部</Text>
+    </View>;
+
     const rowRenderer2 = (type: number | string, data: RowData): React.ReactElement | null => {
+        if (type === ViewTypes.HEADER) {
+            return (
+                Header()
+            )
+        }
+
         return (
             <TouchableOpacity onPress={() => {
                 console.log("click")
             }} style={styles.containerGridLeft}>
-                <XImage model={data} style={styles.image2} resizeMode={'center'}/>
+                <XImage model={data} style={styles.image2} resizeMode={'cover'}/>
             </TouchableOpacity>
         )
     };
-
     return (
         <View style={{flex: 1, width: '100%'}}>
             <View style={{width: '100%', height: 200}}>
                 <RecyclerListView
-                    style={{paddingTop: 10, backgroundColor: "red"}}
+                    ref={scrollRef}
+                    style={{backgroundColor: "red"}}
                     layoutProvider={layoutProvider}
-                    scrollViewProps={{
-                        ref: scrollRef,
-                    }}
+                    // scrollViewProps={{
+                    //     ref: scrollRef,
+                    // }}
                     dataProvider={dataProvider}
                     isHorizontal
-                    onRecreate={(params) => {
+                    onRecreate={(params: OnRecreateParams) => {
                         console.log("onRecreate:", params);
                     }}
-                    onVisibleIndicesChanged={(e) => {
+                    onVisibleIndicesChanged={(e: TOnItemStatusChanged) => {
                         console.log("onVisibleIndicesChanged", e);
                     }}
                     onScroll={(rawEvent: ScrollEvent, offsetX: number, offsetY: number) => {
@@ -107,34 +136,40 @@ const RecyclerViewPage3: React.FC = () => {
             </View>
 
             <Button title={"滑动到0"} onPress={() => {
-                scrollRef?.current._scrollViewRef.scrollTo({
-                    x: 2000,
-                    y: 0,
-                    animated: true
-                });
+                // scrollRef.current?.scrollToIndex(1,true)
+                scrollRef.current?.bringToFocus(5, true)
             }}></Button>
 
-            <View style={{flex: 1, backgroundColor: "blue"}}>
-                <RecyclerListView
-                    style={{paddingTop: 10}}
-                    layoutProvider={layoutProvider2}
-                    dataProvider={dataProvider2}
-                    onRecreate={(params) => {
-                        console.log("onRecreate:", params);
+            <View style={{flex: 1}}>
+                <PullToRefresh
+                    refreshing={refreshing}
+                    onRefresh={() => {
+                        setRefreshing(true);
+                        setTimeout(() => {
+                            setRefreshing(false);
+                        }, 1000)
                     }}
-                    onVisibleIndicesChanged={(e) => {
-                        console.log("onVisibleIndicesChanged", e);
-                    }}
-                    onScroll={(rawEvent: ScrollEvent, offsetX: number, offsetY: number) => {
-                        console.log("onVisibleIndicesChanged", offsetX, offsetY);
-                    }}
-                    renderFooter={() => {
-                        return (
-                            <Text>尾部</Text>
-                        )
-                    }}
-                    rowRenderer={rowRenderer2}
-                />
+                >
+                    <RecyclerListView
+                        layoutProvider={layoutProvider2}
+                        dataProvider={dataProvider2}
+                        renderFooter={() => {
+                            return (
+                                <Text style={{
+                                    width: '100%',
+                                    textAlign: 'center',
+                                    color: 'white',
+                                    height: 50,
+                                    backgroundColor: '#167520'
+                                }}>尾部</Text>
+                            )
+                        }}
+                        rowRenderer={rowRenderer2}
+                    />
+
+                </PullToRefresh>
+
+
             </View>
 
         </View>
@@ -183,5 +218,11 @@ const styles = StyleSheet.create({
         width: WINDOW_WIDTH,
         aspectRatio: 1,
         borderRadius: BORDER_RADIUS,
-    }
+    },
+    header: {
+        backgroundColor: '#f15a54',
+        height: 50,
+        justifyContent: 'center'
+    },
+
 });
